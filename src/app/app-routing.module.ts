2c4da42da683ef529f3efc0e27942051d011cfa8
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-03-25 15:25:56 +0200

    create user view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
 import { UserContainerComponent } from './components/pages/user-container/user-container.component';
 import { AdminContainerComponent } from './components/pages/admin-container/admin-container.component';
+import { DashboardComponent } from './components/pages/user-container/dashboard/dashboard.component';
+import { GridComponent } from './components/pages/user-container/grid/grid.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'work', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   {
     path: 'work',
     component: UserContainerComponent,
-    children: []
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'grid/:id', component: GridComponent },
+    ]
   },
   {
     path: 'admin',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { LoginComponent } from './components/pages/login/login.component';
 import { UserContainerComponent } from './components/pages/user-container/user-container.component';
 import { AdminContainerComponent } from './components/pages/admin-container/admin-container.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './components/pages/user-container/dashboard/dashboard.component';
+import { GridComponent } from './components/pages/user-container/grid/grid.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +40,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     LoginComponent,
     UserContainerComponent,
     AdminContainerComponent,
+    DashboardComponent,
+    GridComponent,
   ],
   imports: [
     BrowserModule,
