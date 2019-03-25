b66ace1f9f268f6518455dc2daddd82d63e02d5c
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-03-25 12:43:08 +0200

    add Form Group for login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/shared/card/card.component';
-import { MatButtonModule, MatCardModule, MatCheckboxModule, MatDividerModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { KanbanGridComponent } from './components/shared/kanban-grid/kanban-grid.component';
 import { KanbanRowComponent } from './components/shared/kanban-grid/kanban-row/kanban-row.component';
@@ -15,7 +23,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/pages/login/login.component';
 import { UserContainerComponent } from './components/pages/user-container/user-container.component';
 import { AdminContainerComponent } from './components/pages/admin-container/admin-container.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -42,6 +50,8 @@ import { FormsModule } from '@angular/forms';
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
+    ReactiveFormsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+  hide = true;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formGroup = this.formBuilder.group({
+        login: ['', Validators.required],
+        password: ['', Validators.required],
+        rememberMe: [false, Validators.required]
+      });
+  }
+
+  login() {
+    if (this.formGroup.valid) {
+      alert(
+`You are almost login in.
+You input
+[login]=${this.formGroup.value.login};
+[password]=${this.formGroup.value.password};
+[rememberMe]=${this.formGroup.value.rememberMe}`
+      );
+    }
   }
 
 }
